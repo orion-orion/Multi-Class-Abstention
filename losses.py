18010e8400c938d9d4d472ee57db4e691fa58ebb
orion-orion <orion-orion@foxmail.com>
2025-11-04 21:47:18 +0900

    feat: update losses & trainer

--- losses.py
+++ losses.py
@@ -33,52 +33,63 @@ class Discriminator(nn.Module):
 
 
 class HingeLoss(nn.Module):
-    def __init__(self, alpha=1):
+    def __init__(self, alpha=1, c=1):
         super(HingeLoss, self).__init__()
         self.alpha = alpha
+        self.c = c
 
     def forward(self, input):
-        return torch.clamp(1 - self.alpha * input, 0)
+        return self.c * torch.clamp(1 - self.alpha * input, 0)
 
 
 class ExponentialLoss(nn.Module):
-    def __init__(self, alpha=1):
+    def __init__(self, alpha=1, c=1):
         super(ExponentialLoss, self).__init__()
         self.alpha = alpha
+        self.c = c
 
     def forward(self, input):
-        return torch.exp(-self.alpha * input)
+        return self.c * torch.exp(-self.alpha * input)
 
 
 class LogisticLoss(nn.Module):
-    def __init__(self, alpha=1):
+    def __init__(self, alpha=1, c=1):
         super(LogisticLoss, self).__init__()
         self.alpha = alpha
+        self.c = c
 
     def forward(self, input):
-        return torch.log1p(self.alpha * input)
+        return self.c * torch.log1p(self.alpha * input)
+
+
+class LabelPred(nn.Module):
+    def __init__(self):
+        super(LabelPred).__init__()
+
+    def forward(self, preds, y):
+        return preds[y]
 
 
 class PredictionMargin(nn.Module):
     def __init__(self):
         super(PredictionMargin, self).__init__()
 
-    def forward(self, preds, y):
-        y = F.one_hot(y).bool()
-        label_score = preds[y]
-        max_score_excl_label = torch.max(
-            preds[~y].reshape(y.shape[0], -1), dim=-1)[0]
-        return self.func_phi(label_score - max_score_excl_label)
+    def forward(self, input_1, input_2, excl_idx=None):
+        return input_1.view(-1, 1) - torch.max(
+            input_2[~excl_idx].view(input_2.shape[0], -1), dim=-1)[0]
 
 
-class PairwiseComparison(nn.Module):
+class PairwiseDiff(nn.Module):
     def __init__(self):
-        super(PairwiseComparison, self).__init__()
+        super(PairwiseDiff, self).__init__()
 
-    def forward(self, preds, y):
-        y = F.one_hot(y).bool()
-        label_score = preds[y]
-        pairwise_diff_matrix = (label_score.reshape(-1, 1) - preds)[~y]
+    def forward(self, input_1, input_2, excl_idx=None):
+        if excl_idx:
+            pairwise_diff_matrix = \
+                input_1.view(-1, 1) - \
+                input_2[~excl_idx].view(input_2.shape[0], -1)
+        else:
+            pairwise_diff_matrix = (input_1.view(-1, 1) - input_2)
         return pairwise_diff_matrix
 
 
--- trainer.py
+++ trainer.py
@@ -13,7 +13,8 @@ from models.p2fcdr.p2fcdr_model import P2FCDR
 from models.ppdm.ppdm_model import PPDM
 from utils.io_utils import ensure_dir
 from utils import train_utils
-from losses import HingeLoss, PredictionMargin, ExponentialLoss, LogisticLoss
+from losses import HingeLoss, PredictionMargin, ExponentialLoss, \
+    LogisticLoss, PairwiseDiff, LabelPred
 
 
 class Trainer(object):
@@ -43,6 +44,7 @@ class ModelTrainer(Trainer):
                                     output_size=data_info["num_cls"])
         self.rejector = resnet1202(in_channels=data_info["n_channels"],
                                    output_size=1)
+        self.c = args.c
         self.checkpoint_dir = args.checkpoint_dir
         self.model_id = (args.model_id if len(args.model_id)
                          > 1 else "0" + args.model_id)
@@ -53,27 +55,29 @@ class ModelTrainer(Trainer):
                 assert (self.psi_type == "exponential")
                 self.psi_1 = ExponentialLoss().to(self.device)
                 self.psi_2 = ExponentialLoss(
-                    args.alpha).to(self.device)
+                    args.alpha, self.c).to(self.device)
             else:
                 self.phi = HingeLoss.to(self.device)
                 assert (self.psi_type in ["exponential", "hinge"])
                 if self.psi_type == "exponential":
                     self.psi = ExponentialLoss(
-                        args.alpha).to(self.device)
+                        args.alpha, self.c).to(self.device)
                 else:
-                    self.psi = HingeLoss(args.alpha).to(self.device)
+                    self.psi = HingeLoss(args.alpha, self.c).to(self.device)
             self.prediction_margin = PredictionMargin()
             self.bce_criterion = nn.BCEWithLogitsLoss().to(self.device)
 
         elif self.method == "Ni+":
             self.ni_et_al_surr_type = args.ni_et_al_surr_type
             if self.ni_et_al_surr_type == "MCS":
+                self.pw_diff = PairwiseDiff().to(self.device)
+                self.label_pred = LabelPred().to(self.device)
                 self.phi = LogisticLoss().to(self.device)
                 self.psi_1 = LogisticLoss().to(self.device)
-                self.psi_2 = LogisticLoss(alpha=1).to(self.device)
+                self.psi_2 = LogisticLoss(alpha=1, c=self.c).to(self.device)
             else:
                 self.phi = LogisticLoss().to(self.device)
-                self.psi = LogisticLoss(alpha=1).to(self.device)
+                self.psi = LogisticLoss(alpha=1, c=self.c).to(self.device)
         else:
             self.params = list(self.predictor.parameters()) + \
                 list(self.discri.parameters())
@@ -95,23 +99,21 @@ class ModelTrainer(Trainer):
         self.optimizer.zero_grad()
 
         X, y = X.to(self.device), y.to(self.device)
+        y = F.one_hot(y).bool()
 
         if self.method == "Ours":
             # preds: (batch_size, num_classe)
             preds = self.predictor(X)
             rej_scores = self.rejector(X)
 
-            margin = self.prediction_margin(preds, y)
-            if self.our_surr_type == "MCS":
-                loss = self.phi(margin) * self.psi_1(rej_scores).mean() \
-                    + self.psi_2(rej_scores).mean()
-            else:
-                loss = self.phi(margin - rej_scores).mean() \
-                    + self.psi_2(rej_scores).mean()
+            loss = self.our_loss_fn(preds, rej_scores, y)
+
         elif self.method == "Ni+":
             # preds: (batch_size, num_classe)
             preds = self.predictor(X)
             rej_scores = self.rejector(X)
+
+            loss = self.ni_et_al_loss_fn(preds, rej_scores, y)
 
         elif "HF" in self.method:
             pass
@@ -121,8 +123,46 @@ class ModelTrainer(Trainer):
         self.step += 1
         return loss.item()
 
-    def hf_loss_fn(self, u, v, neg_v):
-        pass
+    def our_loss_fn(self, preds, rej_scores, y):
+        preds_y = self.label_pred(preds, y)
+        margin = self.prediction_margin(preds_y, preds, y)
+        if self.our_surr_type == "MCS":
+            loss = self.phi(margin) * self.psi_1(rej_scores).mean() \
+                + self.psi_2(rej_scores).mean()
+
+        elif self.our_surr_type == "ACS":
+            loss = self.phi(margin - rej_scores).mean() \
+                + self.psi_2(rej_scores).mean()
+        else:
+            raise Exception(
+                "Method should be one of MCS and ACS")
+        return loss
+
+    def ni_et_al_loss_fn(self, preds, rej_scores, y):
+        preds_y = self.label_pred(preds, y)
+
+        diff = self.pw_diff(preds_y, preds, y)
+        if self.ni_et_al_surr_type == "MCS":
+            loss = self.phi(diff).sum(
+                1) * self.psi_1(rej_scores).mean()\
+                + self.psi_2(rej_scores).mean()
+
+        elif self.ni_et_al_surr_type == "ACS":
+            loss = self.phi(diff - rej_scores)\
+                .sum(-1) * self.psi_1(rej_scores).mean()
+            + self.psi_2(rej_scores).mean()
+
+        elif self.ni_et_al_surr_type == "OVA":
+            loss = self.phi(preds_y).mean() \
+                + self.phi(self.pw_diff(0, preds, y)).sum(-1).mean()
+
+        elif self.ni_et_al_surr_type == "CE":
+            loss = -preds_y.mean()
+            + torch.log(torch.exp(preds)).sum(-1).mean()
+        else:
+            raise Exception(
+                "Method should be one of MCS, ACS, OVA, and CE")
+        return loss
 
     def test_batch(self, X, y):
         """Tests the model for one batch.
@@ -134,14 +174,38 @@ class ModelTrainer(Trainer):
         X, y = X.to(self.device), y.to(self.device)
 
         preds = self.predictor(X)
-        whether_pred = self.rejector(X) > 0
-        X, y = X[whether_pred], y[whether_pred]
 
-        _, pred_labels = torch.max(preds.data, 1)
+        if self.method == "Ours":
+            whether_pred = self.our_decid_fn(X)
+        elif self.method == "Ni+":
+            whether_pred = self.ni_et_al_decid_fn(preds)
+        preds, y = preds[whether_pred], y[whether_pred]
 
-        n_correct = (pred_labels == y).sum().item()
+        _, pred_y = torch.max(preds.data, 1)
+
+        n_correct = (pred_y == y).sum().item()
 
         return n_correct
+
+    def our_decid_fn(self, X):
+        return self.rejector(X) > 0
+
+    def ni_et_al_decid_fn(self, preds):
+        if self.ni_et_al_surr_type == "MCS" \
+                or self.ni_et_al_surr_type == "ACS":
+            whether_pred = torch.max(F.softmax(preds), -1)[0] > 1 - self.c
+
+        elif self.ni_et_al_surr_type == "OVA":
+            whether_pred = torch.max(F.sigmoid(preds), -1)[0] > 1 - self.c
+
+        elif self.ni_et_al_surr_type == "CE":
+            whether_pred = torch.max(F.softmax(preds), -1)[0] > 1 - self.c
+
+        else:
+            raise Exception(
+                "Method should be one of MCS, ACS, OVA, and CE")
+
+        return whether_pred
 
     def save_params(self):
         ensure_dir(self.checkpoint_dir, verbose=True)
